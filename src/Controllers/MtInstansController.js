88a85955313259a6e7eb666b689c552324a4df42
juanBOzuna <70336894+juanBOzuna@users.noreply.github.com>
2023-07-04 19:12:22 +0200

    Update MtInstansController.js

--- src/Controllers/MtInstansController.js
+++ src/Controllers/MtInstansController.js
@@ -4,7 +4,11 @@ import { ObjectID } from 'mongodb';
 import { PROYECT_NAME } from '../config.js'
 import http from 'axios';
 
+
+
+
 class MtInstansController {
+    
     
     static async getInstansByAcountID(DatabaseClient,AcountID){
 
@@ -13,26 +17,14 @@ class MtInstansController {
             let Acount = await AcountsCollection.findOne({_id: ObjectID(AcountID)});
     
             if(Acount.type == MtInstans.mt5){
+
                 let mt5_host_url = Acount.mt5_host_url
                 let connectionID = Acount.connectionID
     
                 if(Acount.mt5_host_url == null || Acount.mt5_host_url == ""){
 
                     mt5_host_url = await this.takeMtInstans(DatabaseClient,MtInstans.mt5)
-                    console.log('host: '+mt5_host_url+' Host ')
-                    const respuesta = (await http.get(`${mt5_host_url}/Search?company=${Acount.broker}`)).data
-                    console.log(respuesta)
-                    const result = respuesta.find(company => company.company == Acount.broker)
-                    console.log(result)
-                    
-                    const result2 = result.results.find(result => result.name === Acount.servidor).access[0];
-                    console.log(result2)
-                    
-                    const [ host, port ] = result2.split(':');
-                    console.log(host, port)
-
-                   
-                   
+                    const [host, port] = (await http.get(`${mt5_host_url}/Search?company=${Acount.broker}`)).data.find(company => company.company === Acount.broker).results.find(result => result.name === Acount.servidor).access[0].split(':');
                     const resp = await http.get(`${mt5_host_url}/Connect?user=${Acount.login}&password=${Acount.password}&host=${host}&port=${port}`);
                     connectionID = resp.data 
                     console.log("[NEW CONNECT]")
@@ -64,9 +56,6 @@ class MtInstansController {
         let MtInstansCollection = DatabaseClient.collection(DBNames.MtInstans);
         let instans = await MtInstansCollection.find({proyect:PROYECT_NAME,type}).toArray()
         const randomHost = instans[Math.floor(Math.random() * instans.length)];
-
-        console.log(instans)
-        console.log(randomHost)
         return randomHost.host
         
     }
